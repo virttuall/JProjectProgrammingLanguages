0d8011c91ffe38006e68712861099763ba81678e
Yeison Garcia <yedgarciago@gmail.com>
2016-05-19 04:57:36 -0500

    add Exp06 and DCL02

--- src/ListenerJava.java
+++ src/ListenerJava.java
@@ -4,45 +4,46 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import javax.naming.spi.DirStateFactory.Result;
+
+
+
 public class ListenerJava extends Java8BaseListener{
 	
 	Java8Parser parser;
 	List<Response> responses;
 	Set<String> arraysName;
+	boolean inAssert;
+	boolean badAssert;
+	int countBooleanForAssert;
 	public ListenerJava(Java8Parser parser)
 	{
 		this.parser = parser;
 		responses = new ArrayList<Response>();
 		arraysName = new TreeSet<String>();
+		inAssert = false;
+		badAssert = false;
+		countBooleanForAssert = 0;
 	}
 	@Override 
 	public void enterAssertStatement(Java8Parser.AssertStatementContext ctx) 
 	{ 
-//		List<Java8Parser.ExpressionContext> listExp = ctx.expression();
-//		for(int i = 0; i < listExp.size(); i++ )
-//		{
-//			System.out.println("hola: "+listExp.get(i).start.toString());
-//		}
-//		System.out.println(ctx.expression());
-//		System.out.println(ctx.getStart().toString());
-//		List<TerminalNode> list = ctx.getTokens(102);
-//		System.out.println(list.toString());
-	}
-	
-	@Override
-	public void enterPrimary(Java8Parser.PrimaryContext ctx) {
-		// TODO Auto-generated method stub
-
+		System.out.println("enterAssertStatement");
+		inAssert = true;
+		
 	}
 	@Override
-	public void enterArrayType(Java8Parser.ArrayTypeContext ctx) {
+	public void exitAssertStatement(Java8Parser.AssertStatementContext ctx) {
 		// TODO Auto-generated method stub
-		//System.out.println(ctx.getText());
-	}
-	@Override
-	public void enterVariableDeclaratorId(Java8Parser.VariableDeclaratorIdContext ctx) {
-		// TODO Auto-generated method stub
-		//System.out.println(ctx.getText());
+		inAssert = false;
+		if ( badAssert )
+		{
+			System.out.println(ctx.getText());
+			Response auxResult = new Response(ctx.getText(), UtilsF.getGoodAssert(ctx.getText(), "AssertBool"+countBooleanForAssert++), "");
+			responses.add(auxResult);
+			badAssert = false;
+		}
+		System.out.println("exitAssertStatement");
 	}
 	@Override
 	public void enterVariableDeclarator(Java8Parser.VariableDeclaratorContext ctx) {
@@ -55,9 +56,9 @@ public class ListenerJava extends Java8BaseListener{
 		super.enterVariableDeclarator(ctx);
 	}
 	@Override
-	public void enterMethodInvocation(Java8Parser.MethodInvocationContext ctx) {
-		// TODO Auto-generated method stub
-		//System.out.println(ctx.getText());
+	public void enterMethodInvocation(Java8Parser.MethodInvocationContext ctx) 
+	{
+		System.out.println("enterMethodInvocation");
 		String aux;
 		for ( final String cad1 : arraysName )
 		{
@@ -75,5 +76,35 @@ public class ListenerJava extends Java8BaseListener{
 			}
 		}
 		super.enterMethodInvocation(ctx);
+	}
+	@Override
+	public void enterMethodInvocation_lf_primary(Java8Parser.MethodInvocation_lf_primaryContext ctx) {
+		// TODO Auto-generated method stub
+		System.out.println("enterMethodInvocation_lf_primary");
+		super.enterMethodInvocation_lf_primary(ctx);
+	}
+	@Override
+	public void enterMethodInvocation_lfno_primary(Java8Parser.MethodInvocation_lfno_primaryContext ctx) {
+		// TODO Auto-generated method stub
+		System.out.println("enterMethodInvocation_lfno_primary");
+		if ( inAssert )
+		{
+			badAssert = true;
+		}
+		super.enterMethodInvocation_lfno_primary(ctx);
+	}
+	@Override
+	public void enterEnhancedForStatement(Java8Parser.EnhancedForStatementContext ctx) {
+		// TODO Auto-generated method stub
+		super.enterEnhancedForStatement(ctx);
+		String cad = ctx.getText();
+		String resp[];
+		if ( !UtilsF.doItHaveFinal(cad))
+		{
+			resp = UtilsF.getGoodAndBad(cad);
+			Response auxResponse = new Response(resp[1], resp[0], "");
+			responses.add(auxResponse);
+		}
+		//System.out.println(ctx.getText());
 	}
 }
--- src/UtilsF.java
+++ src/UtilsF.java
@@ -1,5 +1,7 @@
 
 public class UtilsF {
+	
+	//for EXP02-J
 	public static boolean isArray(String cad)
 	{
 		int count = 0;
@@ -13,5 +15,50 @@ public class UtilsF {
 	public static String nameOfArray(String cad)
 	{
 		return cad.split("=")[0];
+	}
+	
+	//for DCL02-J
+	public static boolean doItHaveFinal(String cad)
+	{
+		String auxCad = cad.substring(4);
+		if (auxCad.startsWith("final"))
+		{
+			return true;
+		}
+		return false;
+	}
+	public static String[] getGoodAndBad(String cad)
+	{
+		String auxCad = cad.substring(4);
+		String bad="", good="";
+		int c = 1;
+		for(int i = 0; i < auxCad.length(); i++ )
+		{
+			if ( auxCad.substring(i, i+1).equals("(") )
+			{
+				c++;
+			}
+			else if ( auxCad.substring(i, i+1).equals(")"))
+			{
+				c--;
+			}
+			if ( c == 0)
+			{
+				bad = "for("+auxCad.substring(0, i+1);
+				good = "for(final"+auxCad.substring(0, i+1);
+				break;
+			}
+		}
+		return new String[]{good, bad};
+	}
+	
+	//for EXP06-J
+	public static String getGoodAssert(String cad, String nameBool)
+	{
+		String auxCad = cad.substring(6);
+		String result = "boolean "+nameBool +" = " + auxCad;
+		result += "\n";
+		result += "assert "+nameBool+";";
+		return result;
 	}
 }
